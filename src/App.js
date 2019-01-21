dacff22a26d478ac869346a8215d2764000a7ab1
Wannita901 <gam_wannita@hotmail..com>
2019-01-21 17:59:33 +0700

    add license agreement

--- src/App.js
+++ src/App.js
@@ -57,6 +57,20 @@ class App extends Component {
         </div>
         <div className="jumbotron" style={{margin:0, minHeight: 300}}>
           <p className="lead">Created by: BooBoo Team</p>
+          <p className="lead small">License Agreement</p>
+          <p className="lead small">
+            This software is a work developed by Ms. Wannita Takerngsaksiri, Mr. Athipat Nampetch and Mr Rujikorn Charakorn from Chulalongkorn University under the provision of Asst. Prof. Thanarat Chalidabhongse
+            under Damaged English text recovery using auto-encoder, which has been supported
+            by the National Electronics and Computer Technology Center (NECTEC), in order to encourage
+            pupils and students to learn and practice their skills in developing software. Therefore, the
+            intellectual property of this software shall belong to the developer and the developer gives
+            NECTEC a permission to distribute this software as an “as is ” and non-modified software for a
+            temporary and non-exclusive use without remuneration to anyone for his or her own purpose or
+            academic purpose, which are not commercial purposes. In this connection, NECTEC shall not be
+            responsible to the user for taking care, maintaining, training or developing the efficiency of this
+            software. Moreover, NECTEC shall not be liable for any error, software efficiency and damages in
+            connection with or arising out of the use of the software.”
+          </p>
         </div>
       </div>
     );
